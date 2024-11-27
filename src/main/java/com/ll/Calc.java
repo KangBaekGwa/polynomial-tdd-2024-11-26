0e076c261c988cff89550dfca7413e7d35a93af3
KangBaekGwa <ksu9801@gmail.com>
2024-11-27 14:17:15 +0900

    Feat : 다항식 계산기 Red Step

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,5 +2,6 @@ package com.ll;
 
 public class Calc {
     public static int run(String expr) {
+        return 0;
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -6,4 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
+
+    @DisplayName("11+12 = ?")
+    @Test
+    void 기본_더하기_계산() {
+        // given
+        String polynomial = "11 + 12";
+
+        // when
+        int result = Calc.run(polynomial);
+
+        // then
+        assertThat(result).isEqualTo(23);
+    }
 }
